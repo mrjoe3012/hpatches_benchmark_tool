c2fcb8a4ea00d5979de441fd21151c5b3d1e333b
Joseph Agrane <josephagrane@gmail.com>
2026-03-27 09:24:18 +0000

    added handler to logger

--- src/hpatches_benchmark/utils/logger.py
+++ src/hpatches_benchmark/utils/logger.py
@@ -1,3 +1,12 @@
 import logging
 
 logger = logging.getLogger("hpatches_benchmark")
+logger.setLevel(logging.INFO)
+
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+ch.setFormatter(formatter)
+
+logger.addHandler(ch)
